3501794f86b527f9fe779adb34b3697a21ab3695
AlfredYuisGood <64243027+AlfredYuisGood@users.noreply.github.com>
2022-08-21 23:26:56 +1000

    Add files via upload

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+	return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+	return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	# return render_template('login.html', error=error)
+	if request.method == "POST":
+		data = request.form.to_dict()
+		print(data)
+		return 'form submitted'
+	else:
+		return 'something wrong'
+
+
+# @app.route('/about.html')
+# def about():
+# 	return render_template('about.html')
+
+# @app.route('/works.html')
+# def work():
+# 	return render_template('works.html')
+
+# @app.route('/contact.html')
+# def contact():
+# 	return render_template('contact.html')
